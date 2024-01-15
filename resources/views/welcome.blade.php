c934b456d26fa5b0abd5c2fe17038bc993fad993
Kenmar Dirchi <64697482+KNMR-GITHUB@users.noreply.github.com>
2024-01-15 15:03:00 +0530

    please completet the home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,20 +8,66 @@
 
     @vite('resources/css/app.css')
 </head>
-<body>
-    @extends('layouts.mainLayout')
-    @section('content')
-        <div>
-            <div>
-                @include('layouts.sidebar')
+<body class="min-h-screen flex flex-col">
+        <div id="nav-bar" class="fixed top-0 h-16 w-full grid grid-cols-[1fr,1fr] bg-white">
+            <!-- Home Page Nav bar -->
+            <div class=" grid grid-cols-[1fr,1fr]">
+                <div class=" flex items-center justify-center">
+                    <a href=""><img src="images/logo.png" class='h-12' alt=""></a>
+                </div>
+                <div class="">
+
+                </div>
             </div>
-            <div>
-                <!-- Include the content inside here -->
+            <div class="grid grid-cols-[1fr,2fr,1.5fr]">
+                <div>
+
+                </div>
+                <div>
+                    <ul class=" flex items-center gap-8 h-16 justify-end">
+                        <li>
+                            <div class=" flex items-center justify-center text-purple-800 font-semibold hover:text-green-400"><a href="">Home</a></div>
+                        </li>
+                        <li>
+                            <div class=" flex items-center justify-center text-purple-800 font-semibold hover:text-green-400"><a href="">Features</a></div>
+                        </li>
+                        <li>
+                            <div class=" flex items-center justify-center text-purple-800 font-semibold hover:text-green-400"><a href="">FAQ</a></div>
+                        </li>
+                        <li>
+                            <div class=" flex items-center justify-center text-purple-800 font-semibold hover:text-green-400"><a href="">Contact</a></div>
+                        </li>
+                        <li>
+                            <div class=" flex items-center justify-center text-purple-800 font-semibold hover:text-green-400"><a href="">Login</a></div>
+                        </li>
+                        <li>
+                            <div class=" flex items-center justify-center hover:text-green-400">
+                                <button class="flex items-center justify-center text-white bg-purple-700 rounded-md pt-2 pb-2 pr-5 pl-5 w-32 hover:bg-green-500">
+                                    Free Signup
+                                </button>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+
+                </div>
             </div>
         </div>
+        <div class="bg-green-500 flex flex-row mt-16 h-screen">
+            <div class="bg-brown-400 grid grid-cols-[1fr,1fr] w-screen">
+                <div>
+                    <h4>Property Management System</h4>
+                    <h1>Manage your Rental properties</h1>
+                    <h3>Get free from hassle of managing your tenants and properties. Maintain your property data efficiently.</h3>
+                    <button>Free Signup →</button>
+                </div>
+                <div class="bg-neutral-400 flex items-center justify-center">
+                    <p class="text">image of some sort</p>
+                </div>
+            </div>
 
-
-    @endsection
+        </div>
 
 </body>
 </html>
