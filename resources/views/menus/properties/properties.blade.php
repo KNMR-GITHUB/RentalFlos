92c23bb71a998f4d8a002e93027413fe1ad7e35e
Kenmar Dirchi <64697482+KNMR-GITHUB@users.noreply.github.com>
2024-01-20 18:40:14 +0530

    justify-between

--- resources/views/menus/properties/properties.blade.php
+++ resources/views/menus/properties/properties.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.mainLayout')
 @section('content')
     <div class="flex flex-col p-10">
-        <div class="flex">
+        <div class="flex justify-between">
             <div>
                 <h1 class="text-2xl font-semibold">Properties</h1>
                 <p class="mt-2 text-gray-400 font-medium">You currently have 0 properties.</p>
             </div>
-            <div>
-                <button class="bg-white text-gray-700 rounded-md pl-4 pr-4 pt-2 pb-2">↓  Export</button>
+            <div class="flex items-center gap-4">
+                <button class="bg-white text-gray-700 rounded-md pl-4 pr-4 pt-2 pb-2 border border-gray-300">↓  Export</button>
                 <button class="bg-purple-800 text-white rounded-md pr-4 pl-4 pt-2 pb-2">+ New Property</button>
             </div>
         </div>
-        <div class="flex bg-white rounded-sm border mt-8 border-gray-300 p-4">
+        <div class="flex justify-evenly bg-white rounded-sm border mt-8 border-gray-300 p-4">
             <h3>Name</h3>
             <h3>Address</h3>
             <h3>Status</h3>
